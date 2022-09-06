fff6287c599db589846cd29444969dde429055bc
cgmccarron <mccarron.christian13@gmail.com>
2022-09-06 10:24:51 -0500

    Starting on tic tac toe logic

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const game_board = () => {
+  let board = new Array(9);
+  const get_board = (num) => {
+    return board[num];
+  };
+};
+
 const spaces = document.querySelectorAll(".spaces");
 
 spaces.forEach((space) => {
   space.addEventListener("click", () => {
     let value = space.getAttribute("id");
-    document.getElementById(value).textContent = "X";
+    document.getElementById(value).textContent = "O";
   });
 });
